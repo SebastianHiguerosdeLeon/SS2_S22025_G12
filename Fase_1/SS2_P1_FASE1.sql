055c3f30168a45c03f9136a51c00e6a10496783f
SebastianHiguerosdeLeon <157766501+SebastianHiguerosdeLeon@users.noreply.github.com>
2025-09-23 17:12:04 -0600

    tablas derivadas

--- Fase_1/SS2_P1_FASE1.sql
+++ Fase_1/SS2_P1_FASE1.sql
@@ -19,26 +19,7 @@ SELECT
   COUNTIF(fare_amount IS NOT NULL) AS no_nulos_tarifa
 FROM `bigquery-public-data.new_york_taxi_trips.tlc_yellow_trips_2022`;
 
--- 3. Crear una tabla copia con datos de enero–marzo, limpia y optimizada
-CREATE OR REPLACE TABLE semi2-proyecto-g12.fase1.taxi_trips_copia
-PARTITION BY DATE(pickup_datetime)
-CLUSTER BY pickup_location_id, dropoff_location_id, payment_type AS
-SELECT *
-FROM bigquery-public-data.new_york_taxi_trips.tlc_yellow_trips_2022
-WHERE data_file_month BETWEEN 1 AND 3
-  AND trip_distance > 0
-  AND total_amount >= 0
-  AND fare_amount >= 0
-  AND passenger_count BETWEEN 1 AND 6;
-
--- 4. Ver las particiones creadas en la tabla copia
-SELECT partition_id, total_rows
-FROM semi2-proyecto-g12.fase1.INFORMATION_SCHEMA.PARTITIONS
-WHERE table_name = 'taxi_trips_copia'
-ORDER BY partition_id
-LIMIT 20;
-
--- 5. Revisar nulos en columnas clave
+-- 3. Revisar nulos en columnas clave
 SELECT
   COUNTIF(pickup_datetime IS NULL) AS nulos_pickup,
   COUNTIF(dropoff_datetime IS NULL) AS nulos_dropoff,
@@ -46,14 +27,14 @@ SELECT
   COUNTIF(trip_distance IS NULL) AS nulos_distancia,
   COUNTIF(total_amount IS NULL) AS nulos_total,
   COUNTIF(payment_type IS NULL) AS nulos_pago
-FROM bigquery-public-data.new_york_taxi_trips.tlc_yellow_trips_2022;
+FROM `bigquery-public-data.new_york_taxi_trips.tlc_yellow_trips_2022`;
 
 
 
 /* ============================================================
-   1. TABLA BASE LIMPIA (Q1) → trips_q1_clean
+   1. TABLA BASE LIMPIA (Q1) → taxi_trips_copia
    ============================================================ */
-CREATE OR REPLACE TABLE `semi2-proyecto-g12.fase1.trips_q1_clean`
+CREATE OR REPLACE TABLE `semi2-proyecto-g12.fase1.taxi_trips_copia`
 PARTITION BY DATE(pickup_datetime)
 CLUSTER BY pickup_location_id, dropoff_location_id, payment_type AS
 SELECT *
@@ -67,13 +48,15 @@ WHERE data_file_month BETWEEN 1 AND 3
 -- Chequeo esquema
 SELECT column_name, data_type
 FROM `semi2-proyecto-g12.fase1.INFORMATION_SCHEMA.COLUMNS`
-WHERE table_name = 'trips_q1_clean';
+WHERE table_name = 'taxi_trips_copia'
+ORDER BY  column_name;
 
 -- Chequeo particiones
 SELECT partition_id, total_rows
-FROM `semi2-proyecto-g12.fase1.INFORMATION_SCHEMA.PARTITIONS`
-WHERE table_name = 'trips_q1_clean'
-ORDER BY partition_id;
+FROM semi2-proyecto-g12.fase1.INFORMATION_SCHEMA.PARTITIONS
+WHERE table_name = 'taxi_trips_copia'
+ORDER BY partition_id
+LIMIT 20;
 
 
 
@@ -90,7 +73,7 @@ SELECT
   ROUND(AVG(trip_distance), 2) AS avg_distance,
   ROUND(AVG(total_amount), 2) AS avg_total,
   ROUND(AVG(tip_amount), 2) AS avg_tip
-FROM `semi2-proyecto-g12.fase1.trips_q1_clean`
+FROM `semi2-proyecto-g12.fase1.taxi_trips_copia`
 GROUP BY month_date, payment_type;
 
 -- Chequeo esquema
@@ -109,26 +92,23 @@ WHERE table_name = 'metrica_mensual_q1';
    3. DISTRIBUCIÓN DE PROPINAS (Q1) → propinas_q1
    ============================================================ */
 CREATE OR REPLACE TABLE `semi2-proyecto-g12.fase1.propinas_q1`
-PARTITION BY month_date
-CLUSTER BY tip_bucket AS
-WITH base AS (
-  SELECT
-    DATE_TRUNC(DATE(pickup_datetime), MONTH) AS month_date,
+PARTITION BY mes
+CLUSTER BY propinas AS 
+WITH datos AS (
+  SELECT 
+    DATE_TRUNC(DATE(pickup_datetime), MONTH) AS mes,
     CASE
       WHEN tip_amount = 0 THEN 'Sin propina'
       WHEN tip_amount <= 2 THEN 'Hasta 2 USD'
       WHEN tip_amount <= 5 THEN '2–5 USD'
       WHEN tip_amount <= 10 THEN '5–10 USD'
       ELSE 'Más de 10 USD'
-    END AS tip_bucket
-  FROM `semi2-proyecto-g12.fase1.trips_q1_clean`
+    END AS propinas
+  FROM `semi2-proyecto-g12.fase1.taxi_trips_copia`
 )
-SELECT
-  month_date,
-  tip_bucket,
-  COUNT(*) AS trips
-FROM base
-GROUP BY month_date, tip_bucket;
+SELECT mes, propinas, COUNT(*) AS viajes
+FROM datos
+GROUP BY mes, propinas;
 
 -- Chequeo esquema
 SELECT column_name, data_type
@@ -143,25 +123,26 @@ WHERE table_name = 'propinas_q1';
 
 
 /* ============================================================
-   4. DEMANDA POR HORA (Q1) → demanda_horaria_q1
+   4. DEMANDA POR HORA (Q1) → demanda_horas_zona_q1
    ============================================================ */
-CREATE OR REPLACE TABLE `semi2-proyecto-g12.fase1.demanda_horaria_q1`
+CREATE OR REPLACE TABLE `semi2-proyecto-g12.fase1.demanda_horas_zona_q1`
 PARTITION BY DATE(pickup_datetime)
-CLUSTER BY pickup_location_id, hour_of_day AS
+CLUSTER BY pickup_location_id, hora_dia AS
 SELECT
-  DATE(pickup_datetime) AS trip_date,
-  EXTRACT(HOUR FROM pickup_datetime) AS hour_of_day,
+  DATE(pickup_datetime) AS fecha_viaje,
+  EXTRACT(HOUR FROM pickup_datetime) AS hora_dia,
   pickup_location_id,
-  COUNT(*) AS trips
-FROM `semi2-proyecto-g12.fase1.trips_q1_clean`
-GROUP BY trip_date, hour_of_day, pickup_location_id;
+  COUNT(*) AS viajes,
+  ANY_VALUE (pickup_datetime) AS pickup_datetime
+FROM `semi2-proyecto-g12.fase1.taxi_trips_copia`
+GROUP BY fecha_viaje, hora_dia, pickup_location_id;
 
 -- Chequeo esquema
 SELECT column_name, data_type
 FROM `semi2-proyecto-g12.fase1.INFORMATION_SCHEMA.COLUMNS`
-WHERE table_name = 'demanda_horaria_q1';
+WHERE table_name = 'demanda_horas_zona_q1';
 
 -- Chequeo particiones
 SELECT partition_id, total_rows
 FROM `semi2-proyecto-g12.fase1.INFORMATION_SCHEMA.PARTITIONS`
-WHERE table_name = 'demanda_horaria_q1';
+WHERE table_name = 'demanda_horas_zona_q1';
